6e5d544129eee821f74d7834fd615d2c960d3e41
DuckMouse <auretswanepoel@gmail.com>
2024-07-20 23:04:12 +1000

    Update ddb-dm-screen.user.js

--- ddb-dm-screen.user.js
+++ ddb-dm-screen.user.js
@@ -21,12 +21,12 @@ console.log("D&DBeyond DM Screen Starting");
 const linkUrlTarget = '.ddb-campaigns-character-card-footer-links-item-view';
 const campaignElementTarget = '.ddb-campaigns-detail-header-secondary';
 
-const rulesUrls = ["https://character-service.dndbeyond.com/character/v4/rule-data", "https://gamedata-service.dndbeyond.com/vehicles/v3/rule-data"];
-const charJSONurlBase = "https://character-service.dndbeyond.com/character/v4/character/";
+const rulesUrls = ["https://character-service.dndbeyond.com/character/v5/rule-data", "https://gamedata-service.dndbeyond.com/vehicles/v3/rule-data"];
+const charJSONurlBase = "https://character-service.dndbeyond.com/character/v5/character/";
 
 const stylesheetUrls = ["https://raw.githack.com/DuckMouse/DNDBeyond-DM-Screen/master/dm-screen.css"]
 
-const gameCollectionUrl = {prefix :"https://character-service.dndbeyond.com/character/v4/game-data/", postfix: "/collection"}
+const gameCollectionUrl = {prefix :"https://character-service.dndbeyond.com/character/v5/game-data/", postfix: "/collection"}
 const optionalRules = {
     "optionalOrigins": {category:"racial-trait", id:"racialTraitId" },
     "optionalClassFeatures": {category:"class-feature", id:"classFeatureId" },
